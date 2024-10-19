9c90ebc3980c01fd96918462820cf5ea82971f5a
Zishan <zishan@hicglobalsolutions.com>
2024-10-19 19:57:35 +0530

    Committing my fist Program

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,5 +1,8 @@
 public class NewClass {
     public static void main(String[] args){
         System.out.println("This is the First Program");
+        int a = 2;
+        int b = 3;
+        int c = a + b;
     }
 }
